b9eee5a3e72433aa4746868440d92febf3642c22
ndrewwm <moore.andrew09@gmail.com>
2023-07-26 11:54:22 -0600

    Load KaTeX in app.html rather than a component

--- src/lib/components/Head.svelte
+++ src/lib/components/Head.svelte
@@ -4,5 +4,4 @@
 
 <head>
   <title>{title} | ndrewwm</title>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.15.2/dist/katex.min.css" integrity="sha384-MlJdn/WNKDGXveldHDdyRP1R4CTHr3FeuDNfhsLPYrq2t0UBkUdK2jyTnXPEK1NQ" crossorigin="anonymous">
 </head>
