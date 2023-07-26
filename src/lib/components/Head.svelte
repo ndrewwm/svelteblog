5a07939a2d63b5f1e4c04b133a2a4cafd17d5d3e
ndrewwm <moore.andrew09@gmail.com>
2023-07-26 11:43:14 -0600

    A component, mostly for blog posts? Fills in title and loads KateX

---
+++ src/lib/components/Head.svelte
@@ -0,0 +1,8 @@
+<script>
+  export let title;
+</script>
+
+<head>
+  <title>{title} | ndrewwm</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.15.2/dist/katex.min.css" integrity="sha384-MlJdn/WNKDGXveldHDdyRP1R4CTHr3FeuDNfhsLPYrq2t0UBkUdK2jyTnXPEK1NQ" crossorigin="anonymous">
+</head>
