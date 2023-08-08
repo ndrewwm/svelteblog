b51eabf8c2a8a5492515864d7bf83d7a5ba1263e
ndrewwm <moore.andrew09@gmail.com>
2023-08-08 17:40:38 -0600

    Hide posts from the page index if they're drafts

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -10,16 +10,19 @@
 
 <ul>
   {#each data.posts as post}
-    <li>
-      <span class="postdt">{post.meta.date}</span>
-      | <a href={post.path}>{post.meta.title}</a>
-    </li>
+    {#if !post.meta.draft}
+      <li>
+        <span class="postdt">{post.meta.date}</span>
+        | <a href={post.path}>{post.meta.title}</a>
+      </li>
+    {/if}
   {/each}
 </ul>
 
 <style>
   .postdt {
     font-family: monospace;
+    font-weight: bolder;
     font-size: larger;
   }
 
