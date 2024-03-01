ed6283f9c02280f797183d07d7ada8796ff5ba48
ndrewwm <moore.andrew09@gmail.com>
2024-03-01 16:38:28 -0700

    Initial pass at a typewriter component

---
+++ src/lib/components/Typewriter.svelte
@@ -0,0 +1,22 @@
+<script>
+  export let phrase;
+</script>
+
+<div class="typing" style="--numchar:20">{phrase}</div>
+
+<style>
+  .typing {
+    animation: typing 1.5s steps(20, end), blink .75s step-end infinite alternate;
+    overflow: hidden;
+    white-space: nowrap;
+  }
+
+  @keyframes typing {
+    from {
+      width: 0;
+    }
+    to {
+      width: 20ch;
+    }
+  }
+</style>
