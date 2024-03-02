fb05b040ceb658cc366ce7ed7200164fb558a49e
ndrewwm <moore.andrew09@gmail.com>
2024-03-02 14:02:37 -0700

    Update plot theme

--- src/lib/assets/theme-ndrewwm.r
+++ src/lib/assets/theme-ndrewwm.r
@@ -12,13 +12,17 @@ theme_ndrewwm <- function() {
   bg_panel <- "#160C28"
   txt_main <- "#E1EFE6"
 
+  update_geom_defaults("hline", aes(color = txt_main))
+  update_geom_defaults("vline", aes(color = txt_main))
+  update_geom_defaults("line", aes(color = txt_main))
   update_geom_defaults("point", aes(color = txt_main))
+  update_geom_defaults("pointrange", aes(color = txt_main))
   update_geom_defaults("boxplot", aes(fill = txt_main, color = txt_main, alpha = 0.2))
   update_geom_defaults("density", aes(color = txt_main, fill = txt_main, alpha = 0.1))
   update_geom_defaults("bar", aes(color = bg_main, fill = txt_main, alpha = 0.9))
   update_geom_defaults("smooth", aes(color = "#EFCB68"))
 
-  theme_minimal(base_size = 18) +
+  theme_minimal(base_size = 15) +
     theme(
       plot.background = element_rect(fill = bg_main),
       panel.border = element_blank(),
@@ -30,6 +34,7 @@ theme_ndrewwm <- function() {
       plot.title = element_text(color = txt_main, face = "bold"),
       plot.title.position = "plot",
       plot.subtitle = element_text(color = txt_main),
+      plot.caption = element_text(color = txt_main, size = 8),
       legend.title = element_text(color = txt_main),
       legend.text = element_text(color = txt_main)
     )
