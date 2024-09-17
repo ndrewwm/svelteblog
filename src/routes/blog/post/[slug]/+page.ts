6b0ebd788b8b232609ac1b9e9215b6a8b0aec19d
ndrewwm <moore.andrew09@gmail.com>
2024-09-16 19:35:05 -0600

    Add redirect

--- src/routes/blog/post/[slug]/+page.ts
+++ src/routes/blog/post/[slug]/+page.ts
@@ -1,4 +1,4 @@
-import { error } from '@sveltejs/kit';
+import { error, redirect } from '@sveltejs/kit';
 
 export async function load({ params }) {
 	try {
@@ -9,6 +9,16 @@ export async function load({ params }) {
 			meta: post.metadata
 		}
 	} catch (e) {
+		if (params.slug.includes("primer-for-linear-regression")) {
+			let url = "https://ndrewwm.com/projects/linear-regression-primer/"
+			if (params.slug.includes("1")) {
+				throw redirect(308, url + "1");
+			} else if (params.slug.includes("2")) {
+				throw redirect(308, url + "2");
+			} else {
+				throw redirect(308, url + "3");
+			}
+		}
 		error(404, `Could not find ${params.slug}`);
 	}
 }
