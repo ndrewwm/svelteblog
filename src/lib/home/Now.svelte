9076c0a068b60122cff613c88d64cb747ed3bd79
Andrew Moore <8618665+ndrewwm@users.noreply.github.com>
2024-09-23 11:59:31 -0600

    Update Now.svelte

--- src/lib/home/Now.svelte
+++ src/lib/home/Now.svelte
@@ -23,6 +23,8 @@
     <li>Working remotely in CU Boulder's OIT, as a senior data engineer</li>
     <li>I'm learning a lot about CI/CD and Snowflake, primarily through standing up dbt-core at work</li>
     <li>Still practicing a bit of German every day</li>
-    <li>About to start my final two lecture courses for my degree: MATH 403 (Linear Algebra) and MATH 465 (Numerical Methods).</li>
+    <li>Working on my final two lecture courses for my degree: MATH 403 (Linear Algebra) and MATH 465 (Numerical Methods).</li>
+    <li>📖 71% <em>The Way We Never Were: American Families and the Nostalgia Trap</em> Stephanie Coontz (1992)</li>
+    <li>📖 29% <em>Butter</em> Asako Yuzuki (2017)</li>
   </ul>
 </div>
