8a69130f639d36f3622b607ba4e04fffa384719d
ndrewwm <moore.andrew09@gmail.com>
2024-10-13 16:43:26 -0600

    Try SAAS mode

--- src/lib/server/database/getMdClient.ts
+++ src/lib/server/database/getMdClient.ts
@@ -1,9 +1,5 @@
 import { DB_MOTHERDUCK_TOKEN } from "$env/static/private";
 import { Database } from "duckdb-async";
 
-const mduck = await Database.create(`md:my_db?motherduck_token=${DB_MOTHERDUCK_TOKEN}`);
-await mduck.exec(`
-  set home_directory='/tmp';
-`);
-
+const mduck = await Database.create(`md:my_db?motherduck_token=${DB_MOTHERDUCK_TOKEN}&saas_mode=true`);
 export { mduck };
