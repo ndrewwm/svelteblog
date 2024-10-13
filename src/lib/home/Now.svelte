32199b7f709cb183daa7b781a2d360d376ec99bc
ndrewwm <moore.andrew09@gmail.com>
2024-10-13 14:16:43 -0600

    Update Now

--- src/lib/home/Now.svelte
+++ src/lib/home/Now.svelte
@@ -19,7 +19,6 @@
   <span style="font-size: smaller">Updated</span> <span>10/9/2024:</span>
 
   <ul>
-    <li>I'm in Las Vegas, NV for <a href="https://coalesce.getdbt.com/">Coalesce 2024!</a></li>
     <li>Living in Boise, ID</li>
     <li>Working remotely in CU Boulder's OIT, as a senior data engineer</li>
     <li>I'm learning a lot about CI/CD and Snowflake, primarily through standing up dbt-core at work</li>
