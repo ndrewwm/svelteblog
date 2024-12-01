dad6fcd330561a267cb993ac5ea9d877cb7dacda
ndrewwm <moore.andrew09@gmail.com>
2024-11-30 19:56:28 -0700

    Clean up blog template

--- src/routes/blog/post/[slug]/+page.svelte
+++ src/routes/blog/post/[slug]/+page.svelte
@@ -11,22 +11,16 @@
   <meta name="description" content={data.meta.description} />
 </svelte:head>
 
-<article>
+<article class="mb-5">
   <hgroup>
     <h1>{data.meta.title}</h1>
-    <div>
-      {data.meta.date} | <a href="/blog">back</a>
-    </div>
+    <h2>{data.meta.date} | <a href="/blog">back</a></h2>
+    <hr class="mt-1 mb-2">
   </hgroup>
-  <hr class="mt-1 mb-2">
   <div class="content">
     <svelte:component this={data.content} />
   </div>
 </article>
-
-<br>
-<br>
-<br>
 
 <style>
   hgroup, h1 {
@@ -35,10 +29,7 @@
   h1 {
     font-size: 40px;
   }
-  hgroup > div {
+  h2 {
     font-size: 25px
   }
-  /* article {
-    font-family: Arial, Helvetica, sans-serif;
-  } */
 </style>
