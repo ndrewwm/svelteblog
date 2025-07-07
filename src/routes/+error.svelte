bac9eadfbbf642caca395f619b15923b632457b9
ndrewwm <moore.andrew09@gmail.com>
2025-07-06 22:17:34 -0600

    Add an error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,28 @@
+<script>
+	import { page } from '$app/state';
+</script>
+
+<div class="content" id="msg">
+  <h1 class="is-size-1">
+    <span id="code">{page.status}</span>
+    {page.error.message}
+  </h1>
+  
+  <div class="is-size-3 block">
+    Sorry! Try again later. 😞
+  </div>
+  
+  <div class="block is-size-3">
+    <a href="/">home</a>
+  </div>
+</div>
+
+<style>
+  #msg {
+    font-family: "Micro 5";
+  }
+
+  #code {
+    font-weight: bolder;
+  }
+</style>
