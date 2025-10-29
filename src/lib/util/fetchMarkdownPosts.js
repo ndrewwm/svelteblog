5993e5fd5edd422252e40a0d0e1d6a7dc6ab05d8
ndrewwm <moore.andrew09@gmail.com>
2025-10-28 22:00:31 -0600

    Allow posts without subfolders

--- src/lib/util/fetchMarkdownPosts.js
+++ src/lib/util/fetchMarkdownPosts.js
@@ -5,7 +5,10 @@ export async function fetchMarkdownPosts() {
   const allPosts = await Promise.all(
     iterablePostFiles.map(async ([path, resolver]) => {
       const { metadata } = await resolver();
-      const postPath = path.replace("/src/lib/posts/", "/blog/post/").replace("/+page.md", "");
+      const postPath = path
+        .replace("/src/lib/posts/", "/blog/post/")
+        .replace("/+page.md", "")
+        .replace(".md", "");
 
       return {
         meta: metadata,
