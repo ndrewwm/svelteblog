39506dd6bc6c9b8b1b3f7575333d220d8f7fca99
ndrewwm <moore.andrew09@gmail.com>
2025-11-16 19:54:29 -0700

    Update now

--- src/lib/components/Now.svelte
+++ src/lib/components/Now.svelte
@@ -1,12 +1,12 @@
 <div class="block mt-2 mb-2">
   <p class="dt mb-1 is-size-5">
-    2025-11-09:
+    2025-11-16:
   </p>
   
   <ul class="is-size-6">
     <li>Living in Boise, ID</li>
     <li>Working remotely in CU Boulder's OIT, as a senior data engineer</li>
-    <li>Currently on vacation in Spain</li>
+    <li>Back from vacation, patiently waiting for ski season</li>
     <li>I've been really enjoying using <a href="https://svelteplot.dev/">SveltePlot</a></li>
     <li>Wrapped up a little <a href="https://github.com/ndrewwm/svelteblog/pull/15">feature</a> to summarize my reading activity</li>
   </ul>
