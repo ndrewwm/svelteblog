a54e085ae176e70fe73c4ede15e4a936587b56c1
ndrewwm <moore.andrew09@gmail.com>
2025-11-27 12:24:39 -0700

    wip

--- src/routes/travel/2025/spain/+page.svelte
+++ src/routes/travel/2025/spain/+page.svelte
@@ -128,7 +128,7 @@
 
 <main class="container">
   <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m70!1m12!1m3!1d1486358.7346110474!2d-5.752462999909664!3d43.31332989280407!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m55!3e0!4m5!1s0xd4e59892c0a4453%3A0x2efe1cc29449f1d5!2s48920%20Portugalete%2C%20Biscay%2C%20Spain!3m2!1d43.318247899999996!2d-3.0211969!4m5!1s0xd4ef3cb6f721e6f%3A0xf2281c1216c70819!2sCastro%20Urdiales%2C%20Spain!3m2!1d43.368822099999996!2d-3.2156354!4m5!1s0xd4eecfb82c6c143%3A0x9fd43576b706143b!2sIslares%2C%20Spain!3m2!1d43.404205499999996!2d-3.2999497!4m5!1s0xd4ec0721d3b6e97%3A0x3b8facb07633b43b!2sLaredo%2C%20Spain!3m2!1d43.408912199999996!2d-3.4316508999999997!4m5!1s0xd4949ae8a6bee71%3A0xd27cdc801682ba96!2sSantander%2C%20Spain!3m2!1d43.4636346!2d-3.8226402!4m5!1s0xd49b3dde66acefb%3A0x520e9f3fdcb58e2!2sPotes%2C%20Spain!3m2!1d43.1535975!2d-4.623611299999999!4m5!1s0xd49cc070931f453%3A0x90dd2682e3c9f685!2sFuente%20D%C3%A9%2C%20Spain!3m2!1d43.1442372!2d-4.8122148!4m5!1s0xd368d6a8d3fdef1%3A0x30872b60589f7d3e!2sOviedo%2C%20Spain!3m2!1d43.3622541!2d-5.8485401!4m5!1s0xd367c66cbf494bf%3A0xbd06d207048d3536!2zR2lqw7NuLCBTcGFpbg!3m2!1d43.5322493!2d-5.660973299999999!5e0!3m2!1sen!2sus!4v1763326189341!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> -->
-   <div class="select">
+   <div class="select mt-2 mb-4">
      <select name="segment" id="segment" bind:value={segment}>
        <option value="bilbao">Bilbao & the Basque Country</option>
        <option value="norte">Camino Norte, Santander</option>
@@ -136,9 +136,6 @@
        <option value="xijon">Oviedo & Gijon</option>
       </select>
     </div>
-      
-   <br>
-   <div class="block"></div>
 
   {#if segment === "bilbao"}
     <Bilbao {metrics} />
