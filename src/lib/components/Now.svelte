48d6e1701b24fc0f30ed2084a36ca0fbfa4eeac6
ndrewwm <moore.andrew09@gmail.com>
2025-12-02 18:08:07 -0700

    update /now

--- src/lib/components/Now.svelte
+++ src/lib/components/Now.svelte
@@ -1,6 +1,6 @@
 <div class="block mt-2 mb-2">
   <p class="dt mb-1 is-size-5">
-    2025-11-22:
+    2025-12-02:
   </p>
   
   <ul class="is-size-6">
@@ -9,7 +9,7 @@
     <li>Back from vacation, patiently waiting for ski season</li>
     <li>Here are some <a href="/travel/2025/spain">photos</a> from Spain. We walked over 100 miles during the trip, logging over 200k steps.</li>
     <li>I've been really enjoying using <a href="https://svelteplot.dev/">SveltePlot</a></li>
-    <li>Wrapped up a little <a href="https://github.com/ndrewwm/svelteblog/pull/15">feature</a> to summarize my reading activity</li>
+    <li>Wrapped up a little <a href="https://github.com/ndrewwm/svelteblog/pull/15">feature</a> to summarize my <a href="/reading/stats">reading activity</a></li>
   </ul>
 </div>
 
