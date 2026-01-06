197974cf075fa7c4ec7a5b852ae2e9ecefe4bdaa
ndrewwm <moore.andrew09@gmail.com>
2026-01-05 17:44:12 -0700

    update now

--- src/lib/components/Now.svelte
+++ src/lib/components/Now.svelte
@@ -1,15 +1,13 @@
 <div class="block mt-2 mb-2">
   <p class="dt mb-1 is-size-5">
-    2025-12-02:
+    2026-01-05:
   </p>
   
   <ul class="is-size-6">
     <li>Living in Boise, ID</li>
     <li>Working remotely in CU Boulder's OIT, as a senior data engineer</li>
-    <li>Back from vacation, patiently waiting for ski season</li>
-    <li>Here are some <a href="/travel/2025/spain">photos</a> from Spain. We walked over 100 miles during the trip, logging over 200k steps.</li>
-    <li>I've been really enjoying using <a href="https://svelteplot.dev/">SveltePlot</a></li>
-    <li>Wrapped up a little <a href="https://github.com/ndrewwm/svelteblog/pull/15">feature</a> to summarize my <a href="/reading/stats">reading activity</a></li>
+    <li>Back at work, had a mostly quiet holiday season</li>
+    <li>Still haven't been skiing at Bogus this year (we could use <a href="/blog/post/waiting-for-snow">some snow</a>)</li>
   </ul>
 </div>
 
