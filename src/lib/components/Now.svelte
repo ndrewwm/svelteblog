b06cfa5d57b0991b0acf2e70a36992acb2adfdbc
Andrew Moore <8618665+ndrewwm@users.noreply.github.com>
2026-02-10 21:43:15 -0600

    Update Now.svelte

--- src/lib/components/Now.svelte
+++ src/lib/components/Now.svelte
@@ -1,13 +1,12 @@
 <div class="block mt-2 mb-2">
   <p class="dt mb-1 is-size-5">
-    2026-01-05:
+    2026-02-10:
   </p>
   
   <ul class="is-size-6">
     <li>Living in Boise, ID</li>
     <li>Working remotely in CU Boulder's OIT, as a senior data engineer</li>
-    <li>Back at work, had a mostly quiet holiday season</li>
-    <li>Still haven't been skiing at Bogus this year (we could use <a href="/blog/post/waiting-for-snow">some snow</a>)</li>
+    <li>Have been enjoying exploring <em>Ciudad Mexico</em></li>
   </ul>
 </div>
 
