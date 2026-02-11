2247d30a39ea7a1f8609fc8f2d63f71f4e688855
Andrew Moore <8618665+ndrewwm@users.noreply.github.com>
2026-02-10 21:44:53 -0600

    Update Now.svelte

--- src/lib/components/Now.svelte
+++ src/lib/components/Now.svelte
@@ -6,7 +6,7 @@
   <ul class="is-size-6">
     <li>Living in Boise, ID</li>
     <li>Working remotely in CU Boulder's OIT, as a senior data engineer</li>
-    <li>Have been enjoying exploring <em>Ciudad Mexico</em></li>
+    <li>Have been enjoying exploring <em>Ciudad Mexico</em>, planning on composing another travelogue once we're back</li>
   </ul>
 </div>
 
