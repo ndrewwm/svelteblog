c763f27a9134e986839721c541ea174f78c1351c
ndrewwm <moore.andrew09@gmail.com>
2026-03-18 22:44:11 -0600

    add analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
   import "$lib/styles/style.css";
+  import { dev } from '$app/environment';
+  import { injectAnalytics } from '@vercel/analytics/sveltekit';
+
+  injectAnalytics({ mode: dev ? 'development' : 'production' });
+
   interface Props {
     children?: import('svelte').Snippet;
   }
