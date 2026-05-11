7f8f7c79b58778085db9a7a2235b8b7292b29438
ndrewwm <moore.andrew09@gmail.com>
2026-05-11 17:23:08 -0600

    update Now.svelte

--- src/lib/components/Now.svelte
+++ src/lib/components/Now.svelte
@@ -1,10 +1,14 @@
 <div class="block mt-2 mb-2">
-  <p class="dt mb-1 is-size-5">2026-03-13</p>
+  <p class="dt mb-1 is-size-5">2026-05-11</p>
   <ul class="is-size-6">
     <li>Living in Boise, ID</li>
     <li>Working remotely in CU Boulder's OIT, as a senior data engineer</li>
-    <li>Skiing while we can, looking forward to treefort</li>
-    <li>I'll be in Austin, TX for a conference at the end of April, if you'd like to connect!</li>
+    <li>Austin TX was neat! Hoping I can attend HEDW with some colleagues again next year.</li>
+    <li>
+      Our place is currently being staged, but we're still living in it.
+      Things feel a bit sterile without all our books and art. We've been cooking a bit less
+      than normal because of this as well.
+    </li>
   </ul>
 </div>
 
