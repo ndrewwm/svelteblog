343bd0dc08081c2c32ea871dd94f7f284f7ba2ab
ndrewwm <moore.andrew09@gmail.com>
2026-09-03 17:27:01 -0600

    run npm audit and use vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { mdsvex } from "mdsvex";
 import relativeImages from "mdsvex-relative-images";
 import remarkFootnotes from "remark-footnotes";
